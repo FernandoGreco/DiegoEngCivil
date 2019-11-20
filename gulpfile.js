6cc69f74d0493585b63c153d824c37af957f883e
Fernando Greco <greco-fernando@hotmail.com>
2019-11-20 19:29:54 -0300

    inplementação do sass

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+
+
+gulp.task('styles', () => {
+    return gulp.src('sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('clean', () => {
+    return del([
+        'css/estilo.css',
+    ]);
+});
+
+
+gulp.task('default', gulp.series(['clean', 'styles']));
